2dbe3f4fe62fb962ccf726a8cd247d47a6ec76d7
hipervee <hipervee@gmailo.com>
2018-06-04 23:34:37 +0530

    add editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { FeaturesModule } from './../features/features.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
